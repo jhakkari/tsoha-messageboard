5d6540e73ebf380a1c8cc36659749fd657e0f8fc
jhakkari <56516743+jhakkari@users.noreply.github.com>
2022-02-02 16:18:50 +0200

    Create first version

---
+++ users.py
@@ -0,0 +1,41 @@
+from db import db
+from flask import session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def login(username, password):
+    sql = "SELECT id, password, role FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if not user:
+        return False
+    else:
+        if check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            session["user_admin"] = user.role
+            return True
+        else:
+            return False
+
+def user_id():
+    return session.get("user_id", 0)
+
+def logout():
+    del session["user_id"]
+    del session["user_admin"]
+
+def register(username, password):
+    hash_value = generate_password_hash(password)
+    try:
+        sql = "INSERT INTO users (username, password, role) VALUES (:username, :password, 0)"
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        db.session.commit()
+    except:
+        return False
+    
+    return login(username, password)
+
+def is_admin():
+    if session.get("user_admin", 0) == 1:
+        return True
+    
+    return False
